1434048453b0e5b1d6e1003f91fb29c4add422e2
axel <axel@neverex1st.io>
2022-03-23 12:17:47 +0800

    init

---
+++ src/Main.ts
@@ -0,0 +1,57 @@
+class Main extends egret.DisplayObjectContainer {
+    public constructor() {
+        super();
+        this.addEventListener(egret.Event.ADDED_TO_STAGE, this.onAddToStage, this);
+    }
+
+    private onAddToStage(event: egret.Event) {
+
+        egret.lifecycle.addLifecycleListener((context) => {
+            context.onUpdate = () => {
+            }
+        })
+
+        egret.lifecycle.onPause = () => {
+            egret.ticker.pause();
+        }
+
+        egret.lifecycle.onResume = () => {
+            egret.ticker.resume();
+        }
+
+        this.runGame().catch(e => {
+            console.log(e);
+        })
+    }
+
+    private async runGame() {
+        await this.loadResource()
+        this.createGameScene();
+        await platform.login();
+        const userInfo = await platform.getUserInfo();
+        console.log(userInfo);
+    }
+
+    private async loadResource() {
+        try {
+            const loadingView = new LoadingUI();
+            this.stage.addChild(loadingView);
+            await RES.loadConfig("resource/default.res.json", "resource/");
+            await RES.loadGroup("preload", 0, loadingView);
+            this.stage.removeChild(loadingView);
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+
+    // 创建游戏场景
+    private createGameScene() {
+        // 趣玩初始化（必须）
+        Qwan.init(this.stage)
+
+        Qwan.stage.jump({route: 'HomeView'})
+        // Qwan.stage.jump({route: 'GameView'})
+    }
+}
